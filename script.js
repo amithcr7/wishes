0db7396034cefa214a0b1d8da311cb120296b09f
amithcr7 <66831674+amithcr7@users.noreply.github.com>
2021-08-08 20:37:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "Eman Sabir" && b=="27-06-1996")
+  	if(a== "Ganapriya" && b=="08-08-1998")
   	{
   	    window.location.href="start.html";
   	}
